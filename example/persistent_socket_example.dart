b34fec7d7e0c520119fd27983521fc72d06f3a15
PointSource <47544779+point-source@users.noreply.github.com>
2023-05-08 13:47:00 -0700

    Init dart package

---
+++ example/persistent_socket_example.dart
@@ -0,0 +1,6 @@
+import 'package:persistent_socket/persistent_socket.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/persistent_socket.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library persistent_socket;
+
+export 'src/persistent_socket_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
